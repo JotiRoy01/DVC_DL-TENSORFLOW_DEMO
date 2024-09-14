96ebeaba00309e12f65a657abf263e78509d593d
JotiRoy01 <jotiroygit@gmail.com>
2024-09-14 13:27:47 +0600

    add two function

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -7,7 +7,16 @@ from src.utils.all_utils import get_timestamp
 
 
 def create_and_save_checkpoint_callbacks(callbacks_dir, checkpoint_dir) :
-    pass
+    checkpoint_file_path = os.path.join(checkpoint_dir, "ckpt_model.h5")
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath = checkpoint_file_path,
+        save_best_only = True
+    )
+
+    ckpt_callbacks_filepath = os.path.join(callbacks_dir, "checkpoint_cb.cb")
+    joblib.dump(checkpoint_callback, ckpt_callbacks_filepath)
+
+    logging.info(f"tensorborad callback is being saved at { ckpt_callbacks_filepath}")
 
 
 def create_and_save_tensorboard_callbacks(callbacks_dir, tensorboard_log_dir) :
