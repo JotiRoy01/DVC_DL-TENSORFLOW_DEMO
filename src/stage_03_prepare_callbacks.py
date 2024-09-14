7f72c2cc41a3bae1d9a28f0c57b86045c256dbf7
JotiRoy01 <jotiroygit@gmail.com>
2024-09-14 12:56:04 +0600

    base file are ready

---
+++ src/stage_03_prepare_callbacks.py
@@ -0,0 +1,60 @@
+from src.utils.all_utils import read_yaml, create_directory
+from src.utils.models import get_VGG_16_model, prepare_model
+from src.utils.callbacks import create_and_save_tensorboard_callbacks, create_and_save_checkpoint_callbacks
+import argparse
+import pandas as pd
+import os
+import shutil
+from tqdm import tqdm
+import logging
+import io
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+log_dir = "logs"
+#create_directory([log_dir])
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir, "running_logs.log"), level=logging.INFO, 
+                    format=logging_str, filemode="a")
+
+
+
+
+def prepare_callbacks(config_path, params_path) :
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+
+    artifacts = config["artifacts"]
+    artifacts_dir = artifacts["ARTIFACTS_DIR"]
+    tensorboard_log_dir = os.path.join(artifacts_dir, artifacts["TENSORBOARD_ROOT_LOG_DIR"])
+    checkpoint_dir = os.path.join(artifacts_dir, artifacts["CHECKPOINT_DIR"])
+    callbacks_dir = os.path.join(artifacts_dir, artifacts["CALLBACKS_DIR"])
+
+    create_directory([
+        tensorboard_log_dir,
+        checkpoint_dir,
+        callbacks_dir
+    ])
+
+create_and_save_tensorboard_callbacks(callbacks_dir, tensorboard_log_dir)
+create_and_save_checkpoint_callbacks(callbacks_dir, checkpoint_dir)
+
+
+
+
+
+
+if __name__ == "__main__" :
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="config/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try :
+        logging.info("stage three starting")
+        prepare_callbacks(config_path=parsed_args.config, params_path = parsed_args.params)
+        logging.info("stage three completed: callbacks are prepare and saved as binary")
+
+    except Exception as e:
+        logging.exception(e)
+        raise e
+    #print(parsed_args)
